99a6e143042ad55a23ba0cbedbf7afd7448ca701
JungEunsu <ujd017@naver.com>
2023-09-05 16:12:54 +0900

    뼈대구축

--- week2/day2/Practice02/Library.java
+++ week2/day2/Practice02/Library.java
@@ -1,3 +1,24 @@
 public class Library {
+
+    int total = 0;
+
+    public Library(int total)
+    {
+        this.total = total;
+    }
+
+    public void add(String string) {
+    }
+
+    public boolean find(String string) {
+        return false;
+    }
+
+    public void delete(String string) {
+    }
+
+    public int getTotalBookCount() {
+        return 0;
+    }
     
 }
