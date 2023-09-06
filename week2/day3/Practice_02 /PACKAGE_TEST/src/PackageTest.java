48e9320393f49d51101aa21286b532128249bafd
JungEunsu <ujd017@naver.com>
2023-09-06 13:43:53 +0900

    두 번째 방법

--- week2/day3/Practice_02 /PACKAGE_TEST/src/PackageTest.java
+++ week2/day3/Practice_02 /PACKAGE_TEST/src/PackageTest.java
@@ -1,9 +1,17 @@
+import java.util.*;
+
+
+
+
 public class PackageTest {
     public static void main(String[] args) throws Exception {
         java.util.List<String> list1 = new java.util.LinkedList<>();
 
 
+        List<Integer> list2 = new ArrayList<>();
+        Collections.sort(list1, list2);
 
-        
+
+
     }
 }
