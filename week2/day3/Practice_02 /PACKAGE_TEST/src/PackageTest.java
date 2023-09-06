74ce4ef2ff5fe0e4af8975057f9e4cc08600ba58
JungEunsu <ujd017@naver.com>
2023-09-06 13:42:09 +0900

    첫 번째 방법

---
+++ week2/day3/Practice_02 /PACKAGE_TEST/src/PackageTest.java
@@ -0,0 +1,9 @@
+public class PackageTest {
+    public static void main(String[] args) throws Exception {
+        java.util.List<String> list1 = new java.util.LinkedList<>();
+
+
+
+        
+    }
+}
