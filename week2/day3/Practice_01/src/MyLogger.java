873ba871ccfccf77268e251dfb18e38ef2c16696
JungEunsu <ujd017@naver.com>
2023-09-06 10:36:44 +0900

    복붙오류 체크

--- week2/day3/Practice_01/src/MyLogger.java
+++ week2/day3/Practice_01/src/MyLogger.java
@@ -1,27 +1,25 @@
-import java.util.logging.Level;
+import java.lang.System.Logger.Level;
 import java.util.logging.Logger;
 
 public class MyLogger {
-
     private final static Logger logger = Logger.getLogger("myLogger");
 
-    public static MyLogger getLogger(){
+    public static Logger getLogger() {
         logger.setLevel(Level.INFO);
         return logger;
     }
-
-
 }
 
-class loggerTest{
 
-    public static int sum(int a, int b){
+
+class LoggerTest {
+    public static int sum(int a, int b) {
         MyLogger.getLogger().info("덧셈 수행 : " + a + " + " + b);
         return a + b;
     }
 
-    public static double devide(int a, int b){
-        if( b == 0) {
+    public static double divide(int a, int b) {
+        if (b == 0) {
             MyLogger.getLogger().warning("나눗셈 에러 발생!");
             throw new ArithmeticException("0으로 나눌 수 없습니다.");
         }
@@ -31,10 +29,7 @@ class loggerTest{
     public static void main(String[] args) {
         int a = 1;
         int b = 2;
-        System.out.println(sum(a,b));
-        System.out.println(devide(a,0));
+        System.out.println(sum(a, b));
+        System.out.println(divide(a, 0));
     }
 }
-
-
-
