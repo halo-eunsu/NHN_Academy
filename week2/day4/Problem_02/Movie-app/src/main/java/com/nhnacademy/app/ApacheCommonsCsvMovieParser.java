f4dbee82d2a84c73e11f30aacf83cf606a522d08
JungEunsu <ujd017@naver.com>
2023-09-07 21:16:12 +0900

    1

--- week2/day4/Problem_02/Movie-app/src/main/java/com/nhnacademy/app/ApacheCommonsCsvMovieParser.java
+++ week2/day4/Problem_02/Movie-app/src/main/java/com/nhnacademy/app/ApacheCommonsCsvMovieParser.java
@@ -1,5 +1,3 @@
-package com.nhnacademy.app;
-
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
@@ -16,30 +14,22 @@ public class ApacheCommonsCsvMovieParser implements MovieParser {
 
     @Override
     public List<Movie> parse(String fileName) throws IOException {
-        InputStream inputStream = getMovieFileAsStream();
-
-        // CSVParser를 생성하고 CSV 파일 파싱
-        CSVParser parser = CSVParser.parse(new InputStreamReader(inputStream, "UTF-8"), CSVFormat.EXCEL);
         List<Movie> movies = new ArrayList<>();
 
-        for (CSVRecord csvRecord : parser) {
-            long movieId = Long.parseLong(csvRecord.get(0));
-            String title = csvRecord.get(1);
-            String[] genreArray = csvRecord.get(2).split("\\|");
-            Set<String> genres = new HashSet<>();
-            for (String genre : genreArray) {
-                genres.add(genre);
-            }
+        try (InputStream inputStream = getMovieFileAsStream();
+             InputStreamReader reader = new InputStreamReader(inputStream, "UTF-8");
+             CSVParser parser = CSVParser.parse(reader, CSVFormat.EXCEL)) {
 
-            Movie movie = new Movie(movieId, title, genres);
-            movies.add(movie);
-            
+            for (CSVRecord csvRecord : parser) {
+                long movieId = Long.parseLong(csvRecord.get(0));
+                String title = csvRecord.get(1);
+                String[] genreArray = csvRecord.get(2).split("\\|");
+                Set<String> genres = new HashSet<>(Arrays.asList(genreArray));
+                Movie movie = new Movie(movieId, title, genres);
+                movies.add(movie);
+            }
         }
 
         return movies;
-    }
-
-    public InputStream getMovieFileAsStream() {
-        return getClass().getClassLoader().getResourceAsStream(MOVIE_FILE_NAME);
     }
 }
