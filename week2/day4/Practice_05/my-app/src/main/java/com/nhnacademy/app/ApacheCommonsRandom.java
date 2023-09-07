952a09b797514eb23629175ab646ddc716a5e853
JungEunsu <ujd017@naver.com>
2023-09-07 14:31:06 +0900

    Dependency

--- week2/day4/Practice_05/my-app/src/main/java/com/nhnacademy/app/ApacheCommonsRandom.java
+++ week2/day4/Practice_05/my-app/src/main/java/com/nhnacademy/app/ApacheCommonsRandom.java
@@ -1,4 +1,5 @@
 package com.nhnacademy.app;
+import java.util.Random;
 import org.apache.commons.math3.random.RandomGenerator;
 
 
@@ -6,5 +7,12 @@ public class ApacheCommonsRandom {
 
     public static void main(String[] args) {
         
+        Random r = new Random(0);
+        
+
+        for(int i = 0; i< 10; i++){
+            int rand = r.nextInt(1000) + 1;
+            System.out.println("random : " + rand);
+        }
     }
 }
--- week2/day4/Practice_05/my-app/src/main/java/com/nhnacademy/app/JavaRandom.java
+++ week2/day4/Practice_05/my-app/src/main/java/com/nhnacademy/app/JavaRandom.java
@@ -2,5 +2,12 @@ package com.nhnacademy.app;
 import java.util.Random;
 
 public class JavaRandom {
-    
+    public static void main(String[] args) {
+        
+        Random r = new Random(0);
+        for(int i = 0; i<10; i++){
+            int rand = r.nextInt(100) + 1;
+            System.out.println("java random: " + rand);
+        }
+    }
 }
