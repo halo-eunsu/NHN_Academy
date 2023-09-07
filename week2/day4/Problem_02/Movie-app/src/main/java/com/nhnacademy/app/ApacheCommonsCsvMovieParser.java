014ae35320959769ef477230a8138a918c2ba424
JungEunsu <ujd017@naver.com>
2023-09-07 21:00:14 +0900

    2

--- week2/day4/Problem_02/Movie-app/src/main/java/com/nhnacademy/app/ApacheCommonsCsvMovieParser.java
+++ week2/day4/Problem_02/Movie-app/src/main/java/com/nhnacademy/app/ApacheCommonsCsvMovieParser.java
@@ -3,7 +3,13 @@ package com.nhnacademy.app;
 import java.io.IOException;
 import java.util.List;
 import org.apache.commons.csv.*;
-
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class ApacheCommonsCsvMovieParser implements MovieParser {
 
@@ -18,6 +24,13 @@ public class ApacheCommonsCsvMovieParser implements MovieParser {
     CSVParser parser = CSVParser.parse(csvData, CSVFormat.EXCEL);
     for (CSVRecord csvRecord : parser) {
         ...
+    }
+    @Override
+
+    
+    public InputStream getMovieFileAsStream() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMovieFileAsStream'");
     }
 
 
