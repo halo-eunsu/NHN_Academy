6abd84e87e685963ba9ab52a998f2aa9586f63c8
halo-eunsu <ujd017@naver.com>
2023-09-12 16:55:00 +0900

    Consumer에 thread를 만든 이유

--- week3/day2/Tryouts/PCSolution/src/main/java/com/nhnacademy/PCSolution01.java
+++ week3/day2/Tryouts/PCSolution/src/main/java/com/nhnacademy/PCSolution01.java
@@ -1,14 +1,19 @@
 package main.java.com.nhnacademy;
 
 public class PCSolution01 {
-   
+
     public static void main(String[] args) {
         Store store = new Store(3);
         Producer producer = new Producer(store);
         Consumer consumer = new Consumer("C1", store);
-        
-        producer.start();       
-        consumer.start();       
 
+        Thread consumerThread = new Thread(consumer, consumer.getName());
+
+        producer.start();
+        consumerThread.start();
+
+        while (true) {
+            consumerThread.getState();
+        }
     }
 }
