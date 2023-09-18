cbddf130bc8f09f26a56107d897891eaf93f5a3f
halo-eunsu <ujd017@naver.com>
2023-09-18 15:50:07 +0900

    1

---
+++ week4/day1 0918/Practice/network_programming/src/main/java/com/nhnacademy/Quiz05.java
@@ -0,0 +1,30 @@
+package com.nhnacademy;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+public class Quiz05 {
+
+    
+    public static void main(String[] args) {
+        byte[] buffer = new byte[2048];
+        try (Socket socket = new Socket("localhost", 12345);
+            InputStream inputStream = socket.getInputStream();
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream)
+        ) {
+           int length;
+           while((length = bufferedInputStream.read(buffer)) >= 0){
+            String line = new String(buffer, 0, length).trim();
+            System.out.println("Read : " + new String(buffer, 0, length));
+            //System.out.println("Read : " + new String(buffer, 0, length));
+           }
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+}
