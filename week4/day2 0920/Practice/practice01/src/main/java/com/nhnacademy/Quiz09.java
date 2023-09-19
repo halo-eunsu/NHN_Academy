b5f62bf202e2ec55aad9e24d5df42a7fa8895b95
halo-eunsu <ujd017@naver.com>
2023-09-19 11:07:29 +0900

    1

--- week4/day2 0920/Practice/practice01/src/main/java/com/nhnacademy/Quiz09.java
+++ week4/day2 0920/Practice/practice01/src/main/java/com/nhnacademy/Quiz09.java
@@ -1,6 +1,11 @@
 package com.nhnacademy;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,17 +13,30 @@ public class Quiz09 {
     
 
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    try(ServerSocket serverSocket = new ServerSocket(port);
+        Socket socket = new Socket(serverSocket.accept());
+        BufferedReader socketIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        BufferedWriter socketOut = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        BufferedReader terminalIn = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter terminalOut = new BufferedWriter(new OutputStreamWriter(System.out));
         
+        ){
+            System.out.println("접속에 성공하였습니다.");
+            String line;
 
-        ServerSocket Socket = new ServerSocket(port);
+            while((line = terminalIn.readLine()) != null){
+                socketOut.write(line + "\n");
+                socketOut.flush();
 
-        
+                line = socketIn.readLine();
+                System.out.println(line);
+            }
 
-
-        BufferedInputStream
-        BufferedOut
-
+    }catch(IOException ignore) {
+        System.out.println("연결에 실패하였습니다.");
+    }
+       
     }
 
 }
