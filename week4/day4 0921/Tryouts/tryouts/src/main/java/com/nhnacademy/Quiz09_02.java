c7ec22fa0a6b7ae0af31b00c1daf135cc96935ee
halo-eunsu <ujd017@naver.com>
2023-09-21 14:52:33 +0900

    1

---
+++ week4/day4 0921/Tryouts/tryouts/src/main/java/com/nhnacademy/Quiz09_02.java
@@ -0,0 +1,20 @@
+package com.nhnacademy;
+
+import org.apache.commons.cli.Options;
+
+public class Quiz09_02 {
+    
+
+    public static void main(String[] args) {
+        
+
+        Options options= new Options();
+
+        String ac = "ac"; 
+
+        options.addOption("ac", "acception", false, "상태");
+        options.addOption("p", "path", false, "경로");
+        
+
+    }
+}
