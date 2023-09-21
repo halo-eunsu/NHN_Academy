c3f380013eef4f84fdca6672a7aea81a853b9262
halo-eunsu <ujd017@naver.com>
2023-09-21 10:06:15 +0900

    1

---
+++ week4/day4 0921/Solutions/solutions/src/main/java/com/nhnacademy/Exam01.java
@@ -0,0 +1,38 @@
+package com.nhnacademy;
+
+public class Exam01 {
+    public static void main(String[] args) {
+       
+       
+    Integer[] arr = {5,7,2,3,4,10,1,6};
+       
+       
+       
+        for(int i = 1; i < arr.length; i++){
+
+            for(int j = 0; j < arr.length; j++){
+
+                
+                if(arr[i] > arr[j]){
+                    swap(arr, j, i);
+                    
+                }
+
+            }
+
+        }
+
+        for(int a : arr){
+            System.out.println(a);
+        }
+
+
+    }
+
+
+    private static void swap(Integer[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[j] = arr[i];
+        arr[i] = temp;
+    }
+}
