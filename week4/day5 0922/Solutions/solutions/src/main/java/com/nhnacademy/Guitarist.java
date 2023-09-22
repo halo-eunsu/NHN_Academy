30f3aca491e8a603ef697c3a84112f8bdf9d80c8
halo-eunsu <ujd017@naver.com>
2023-09-22 15:13:31 +0900

    1

---
+++ week4/day5 0922/Solutions/solutions/src/main/java/com/nhnacademy/Guitarist.java
@@ -0,0 +1,103 @@
+package com.nhnacademy;
+
+import java.lang.module.ModuleDescriptor.Builder;
+
+public class Guitarist {
+
+    private int no;
+    private String name;
+    private String teamName;
+    private String guitar;
+
+
+
+    // 방법 1 : 연산자 오버로딩
+    public Guitarist(int no) {
+        this.no = no;
+    }
+
+    public Guitarist(int no, String name) {
+        this(no);
+        this.name = name;
+    }
+
+    public Guitarist(int no, String name, String teamName) {
+        this(no, name);
+        this.teamName = teamName;
+
+    }
+
+    // 방법 2
+    public int getNo() {
+        return this.no;
+    }
+
+    public void setNo(int value) {
+        this.no = value;
+    }
+
+
+    public Guitarist(Builder builder) {
+        this.no = builder.no;
+        this.name = builder.name;
+        this.teamName = builder.teamName;
+        this.guitar = builder.guitar;
+    }
+    public String toString() { 
+        return this.no + ", " + this.name + ", " + this.teamName + ", " + this.guitar;
+    }
+    // 번거로운 위 방법을 대체할 방법
+
+    public static class Builder {
+        private int no;
+        private String name;
+        private String teamName;
+        private String guitar;
+
+        public Builder no(int no) {
+            this.no = no;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder teamName(String teamName) {
+            this.teamName = teamName;
+            return this;
+        }
+
+
+        public Builder guitar(String guitar) {
+            this.guitar = guitar;
+            return this;
+        }
+
+        public Guitarist build() {
+            return null;
+        }
+
+
+
+    }
+
+}
+
+
+class Test {
+    public static void main(String[] args){
+
+        Guitarist guitarist = new Guitarist.Builder()
+            .no(1)
+            .name("Randy Rhoads")
+            .teamName("Quite Notes")
+            .guitar("guitar")
+            .build();
+
+
+
+        System.out.println(guitarist);
+    }
+}
---
+++ week4/day5 0922/Solutions/solutions/src/main/java/com/nhnacademy/StreamSample.java
@@ -0,0 +1,52 @@
+package com.nhnacademy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamSample {
+
+
+    public static int[] getIntegers(int size){
+        int[] array = new int[100];
+        for(int i = 0; i < array.length ; i++){
+            array[i] = i;
+
+        }
+        return array;
+    }
+    // public static Stream<Integer>
+
+    public static List<Integer> getIntegerList(int size){
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < size; i++){
+            list.add(i);
+        }
+        return list;
+    }
+
+    public static List<Guitarist> getGuitarist(){
+        List<Guitarist> list = new ArrayList<>();
+        list.add(new Guitarist.Builder()
+            .no(1)
+
+        );
+
+    }
+    
+}
+
+class Test { 
+    public static void main(String[] args) {
+        int[] array = StreamSample.getIntegers(100);
+
+        for (int i : array) {
+            System.out.println(i);
+        }
+
+        List<Integer> list = StreamSample.getIntegerList(100);
+
+        for(int i : list) {
+            System.out.println(i);
+        }
+    }
+}
