df8456e6be69d256698b47bad554e1a659fb8e5e
halo-eunsu <ujd017@naver.com>
2023-10-10 14:04:10 +0900

    생성자

--- week7/day1 1010/Practice/xflow/src/main/java/com/nhnacademy/Node.java
+++ week7/day1 1010/Practice/xflow/src/main/java/com/nhnacademy/Node.java
@@ -3,13 +3,20 @@ package com.nhnacademy;
 public abstract class Node {
     String id;
 
+
+    //최소 protected private는 안됨
+    protected Node(String id){
+        this.id = id;
+    }
+
+
     public String getId() {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
+
+
+
 
     
 }
