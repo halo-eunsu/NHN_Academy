15fe5da603334ff74cbc513b2d9d8e658a2b13f8
halo-eunsu <ujd017@naver.com>
2023-10-10 14:05:05 +0900

    name setter

--- week7/day1 1010/Practice/xflow/src/main/java/com/nhnacademy/Node.java
+++ week7/day1 1010/Practice/xflow/src/main/java/com/nhnacademy/Node.java
@@ -2,7 +2,7 @@ package com.nhnacademy;
 
 public abstract class Node {
     String id;
-
+    String name;
 
     //최소 protected private는 안됨
     protected Node(String id){
@@ -12,6 +12,14 @@ public abstract class Node {
 
     public String getId() {
         return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
 
