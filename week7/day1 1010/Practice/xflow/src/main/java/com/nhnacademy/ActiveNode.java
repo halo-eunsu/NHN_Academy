590600d4c5547d0035f0800bf567ef6635e771d1
halo-eunsu <ujd017@naver.com>
2023-10-10 16:05:35 +0900

    setter getter 추가

--- week7/day1 1010/Practice/xflow/src/main/java/com/nhnacademy/ActiveNode.java
+++ week7/day1 1010/Practice/xflow/src/main/java/com/nhnacademy/ActiveNode.java
@@ -3,6 +3,7 @@ package com.nhnacademy;
 public class ActiveNode extends Node implements Runnable {
 
     Thread thread;
+    int interval = 1;
 
     protected ActiveNode() {
         super();
@@ -12,14 +13,24 @@ public class ActiveNode extends Node implements Runnable {
         super(id);
     }
 
+    //setter getter 추가
+    public void setInterval(int interval) {
+        this.interval = interval;
+    }
+
+    public int getInterval() {
+        return interval;
+    }
+
+
 
 
     // run 구조
     protected void preprocess() {
-        
+
     }
 
-    public void start(){
+    public void start() {
         thread = new Thread(this);
         thread.start();
     }
@@ -28,23 +39,29 @@ public class ActiveNode extends Node implements Runnable {
 
     }
 
-    protected void mainp(){
+    protected void main() {
 
     }
 
-    //살아있는 동안 도세요...
-    public boolean isAlive(){
+    // 살아있는 동안 도세요...
+    public boolean isAlive() {
         return !thread.isInterrupted();
     }
 
 
-    //run 구조 : 교재의 'ActiveNode class는 아래와 같은 상태를 가질 수 있다.'  부분 그림
+    // run 구조 : 교재의 'ActiveNode class는 아래와 같은 상태를 가질 수 있다.' 부분 그림
     @Override
     public void run() {
-        
+
         preprocess();
-        while(isAlive()){
-            main();
+        while (isAlive()) {
+            try {
+                main();
+                Thread.sleep(interval);
+            } catch (Exception e) {
+                Thread.currentThread().interrupt();
+            }
+
         }
         postprocess();
     }
