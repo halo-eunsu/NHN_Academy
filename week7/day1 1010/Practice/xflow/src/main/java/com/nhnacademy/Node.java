2ca0934f57350f00d8b55ce37180f063add42ed5
halo-eunsu <ujd017@naver.com>
2023-10-10 15:14:14 +0900

    nodeTest 완료

--- week7/day1 1010/Practice/xflow/src/main/java/com/nhnacademy/Node.java
+++ week7/day1 1010/Practice/xflow/src/main/java/com/nhnacademy/Node.java
@@ -4,12 +4,13 @@ public abstract class Node {
     
     String id;
     String name = "";
-
+    //모든 Node가 공유하는 count
+    static int count;
 
     //최소 protected private는 안됨
     protected Node(){
         //고유번호 생성
-        this(String.valueOf(System.currentTimeMillis()));
+        this(String.valueOf(System.currentTimeMillis() + (++count)));
     }
 
     protected Node(String id){
@@ -26,6 +27,10 @@ public abstract class Node {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public static int getTotalCount(){
+        return count;
     }
 
     
