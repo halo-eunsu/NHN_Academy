a193464b6457b892f0468578257a44f6755e7949
halo-eunsu <ujd017@naver.com>
2023-10-10 14:02:04 +0900

    getter, setter

--- week7/day1 1010/Practice/xflow/src/main/java/com/nhnacademy/Node.java
+++ week7/day1 1010/Practice/xflow/src/main/java/com/nhnacademy/Node.java
@@ -3,5 +3,13 @@ package com.nhnacademy;
 public abstract class Node {
     String id;
 
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    
 }
